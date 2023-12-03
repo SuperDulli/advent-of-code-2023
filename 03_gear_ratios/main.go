ed8d23b0b59730d284c4003aa609ad59fd4f98cd
chelmerd <chelmerd@student.42wolfsburg.de>
2023-12-03 15:14:25 +0000

    add day 3 part 2

--- 03_gear_ratios/main.go
+++ 03_gear_ratios/main.go
@@ -21,6 +21,30 @@ func main() {
 		sum += number
 	}
 	fmt.Println(sum)
+
+	// part 2
+	// get all numbers that are touching a * and remember the stars position
+	gearParts := getGearParts(matrix)
+	var sum2 int
+	for i, gearPart := range gearParts {
+		if gearPart.checked {
+			continue
+		}
+		gearParts[i].checked = true
+		gearNumbers := []int{gearPart.number}
+		for j, gearPart2 := range gearParts {
+			if i == j || gearPart.pos != gearPart2.pos || gearPart2.checked {
+				continue
+			}
+			gearParts[j].checked = true
+			gearNumbers = append(gearNumbers, gearPart2.number)
+		}
+		// if exactly two numbers are touching the same gear add their ratio to the sum
+		if len(gearNumbers) == 2 {
+			sum2 += gearNumbers[0] * gearNumbers[1]
+		}
+	}
+	println(sum2)
 }
 
 func getParts(matrix [][]string) []string {
@@ -79,4 +103,71 @@ func isInbound(matrix [][]string, x int, y int) bool {
 	}
 	width := len(matrix[x])
 	return y < width
+}
+
+// part 2
+
+type cord struct {
+	x int
+	y int
+}
+
+type gearPart struct {
+	number  int
+	pos     cord
+	checked bool
+}
+
+func getGearParts(matrix [][]string) []gearPart {
+	var parts []gearPart
+	directions := [][]int{
+		{-1, -1}, {-1, 0}, {-1, 1},
+		{0, -1}, {0, 1},
+		{1, -1}, {1, 0}, {1, 1},
+	}
+	for row, line := range matrix {
+		var part []string
+		isTouching := false
+		gearPos := cord{}
+		for col, char := range line {
+			if _, err := strconv.Atoi(char); err != nil {
+				if len(part) > 0 && isTouching {
+					number, err := strconv.Atoi(strings.Join(part, ""))
+					if err != nil {
+						log.Fatal(err)
+					}
+					parts = append(parts, gearPart{number: number, pos: gearPos})
+				}
+				part = []string{}
+				isTouching = false
+				gearPos = cord{}
+				continue
+			}
+			part = append(part, char)
+			if isTouching {
+				continue
+			}
+			for _, dir := range directions {
+				dx, dy := dir[0], dir[1]
+				x := row + dx
+				y := col + dy
+				if !isInbound(matrix, x, y) {
+					continue
+				}
+				if "*" == matrix[x][y] {
+					isTouching = true
+					gearPos.x = x
+					gearPos.y = y
+				}
+			}
+		}
+		if len(part) > 0 && isTouching {
+			number, err := strconv.Atoi(strings.Join(part, ""))
+			if err != nil {
+				log.Fatal(err)
+			}
+			parts = append(parts, gearPart{number: number, pos: gearPos})
+		}
+	}
+	return parts
 }
