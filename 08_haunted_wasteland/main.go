b3a9556f752111b368b2b88df8029e8abc23bd68
Chris Helmerding <chris.helmerding@capgemini.com>
2023-12-08 15:59:25 +0000

    add day 8 part 2 brute force - runtime unknown

--- 08_haunted_wasteland/main.go
+++ 08_haunted_wasteland/main.go
@@ -12,6 +12,8 @@ type node struct {
 }
 
 func main() {
+	// lines := util.ReadLines("example3.txt")
+
 	lines := util.ReadLines(os.Args[1])
 	directions := lines[0]
 	nodes := make(map[string]node)
@@ -26,7 +28,7 @@ func main() {
 	pos := "AAA"
 	steps := 0
 	for pos != "ZZZ" {
-		move := directions[steps % len(directions)]
+		move := directions[steps%len(directions)]
 		if move == 'L' {
 			pos = nodes[pos].left
 		} else {
@@ -36,4 +38,39 @@ func main() {
 		steps++
 	}
 	fmt.Println(steps)
+
+	// part 2
+
+	var positions []string
+	for start := range nodes {
+		if start[2] == 'A' {
+			positions = append(positions, start)
+		}
+	}
+
+	steps = 0
+	for !allFinished(positions) {
+		fmt.Println(positions)
+		for i, pos := range positions {
+			move := directions[steps%len(directions)]
+			if move == 'L' {
+				pos = nodes[pos].left
+			} else {
+
+				pos = nodes[pos].right
+			}
+			positions[i] = pos
+		}
+		steps++
+	}
+	fmt.Println(steps)
+}
+
+func allFinished(positions []string) bool {
+	for _, pos := range positions {
+		if pos[2] != 'Z' {
+			return false
+		}
+	}
+	return true
 }
