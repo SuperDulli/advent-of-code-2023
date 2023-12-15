1579c854f72e85ec522f6f055ab1c61d767c3849
chelmerd <chelmerd@student.42wolfsburg.de>
2023-12-15 14:07:06 +0000

    add day 15 part 2

--- 15_lens_library/main.go
+++ 15_lens_library/main.go
@@ -4,8 +4,15 @@ import (
 	"aoc2023/util"
 	"fmt"
 	"os"
+	"regexp"
+	"slices"
 	"strings"
 )
+
+type lens struct {
+	label string
+	value int
+}
 
 func main() {
 	initSequence := util.ReadLines(os.Args[1])[0]
@@ -13,11 +20,47 @@ func main() {
 		return r == ','
 	})
 
+	labels := make(map[string]int, 0)
+	boxes := make([][]lens, 256)
+
 	sum := 0
 	for _, step := range steps {
+		label := regexp.MustCompile("[a-z]+").FindString(step)
+		_, ok := labels[label]
+		if !ok {
+			labels[label] = hash(label)
+		}
+		n := labels[label]
+		switch regexp.MustCompile("[^a-z]+").FindString(step)[0] {
+		case '=':
+			index := slices.IndexFunc(boxes[n], func(l lens) bool {
+				return l.label == label
+			})
+			number := util.ConvertToNumber(regexp.MustCompile("[0-9]+").FindString(step))
+			if index >= 0 {
+				boxes[n][index].value = number
+			} else {
+				boxes[n] = append(boxes[n], lens{label, number})
+			}
+		case '-':
+			indexToRemove := slices.IndexFunc(boxes[n], func(l lens) bool {
+				return l.label == label
+			})
+			if indexToRemove >= 0 {
+				boxes[n] = util.RemoveStable(boxes[n], indexToRemove)
+			}
+		}
 		sum += hash(step)
 	}
 	fmt.Println(sum)
+
+	focusingPower := 0
+	for n, box := range boxes {
+		for i, slot := range box {
+			focusingPower += (n + 1) * (i + 1) * slot.value
+		}
+	}
+	fmt.Println(focusingPower)
 }
 
 func hash(s string) int {
