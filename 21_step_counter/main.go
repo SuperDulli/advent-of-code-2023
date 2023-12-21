3f77a67fc7ccaebbdd4b3b01b3b65ea948154b6d
Chris Helmerding <chris.helmerding@capgemini.com>
2023-12-21 15:10:32 +0000

    add day 21 part 2 examples work

--- 21_step_counter/main.go
+++ 21_step_counter/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"aoc2023/util"
 	"fmt"
+	"log"
 	"os"
 )
 
@@ -16,78 +17,85 @@ type QueuePos struct {
 }
 
 func main() {
+	if len(os.Args) != 3 {
+		log.Fatal("Usage: file steps")
+	}
 	garden := util.GetCharMatrix(os.Args[1])
 
 	start := Vector{}
 
-	distances := make([][]int, len(garden))
-	for i := range distances {
-		distances[i] = make([]int, len(garden[i]))
-		for j := range distances[i] {
-			distances[i][j] = -1
+	distances := make(map[Vector]int)
+	for i := range garden {
+		for j := range garden[i] {
 			if garden[i][j] == "S" {
+				garden[i][j] = "."
 				start = Vector{j, i}
-				continue
+				break
 			}
 		}
 	}
 
-
 	// flood fill the garden to get the distance to each tile from the start
-	steps := 64
+	steps := util.ConvertToNumber(os.Args[2])
 
 	queue := []QueuePos{{0, start}}
 	for len(queue) > 0 {
 		pos := queue[0]
 		queue = queue[1:]
-		if pos.distance > steps{
+		if pos.distance > steps {
 			break
 		}
-		if distances[pos.pos.y][pos.pos.x] != -1 {
+		_, ok := distances[pos.pos]
+		if ok {
 			continue
 		}
-		distances[pos.pos.y][pos.pos.x] = pos.distance
-		neighbors := neighbors(pos.pos, garden, distances)
+		distances[pos.pos] = pos.distance
+		neighbors := neighbors(pos.pos, garden)
 
 		for _, neighbor := range neighbors {
 			queue = append(queue, QueuePos{pos.distance + 1, neighbor})
 		}
 
 	}
-	fmt.Println(reachable(distances))
+	fmt.Println(reachable(distances, steps))
 
 }
 
-func neighbors(pos Vector, garden [][]string, distances [][]int) []Vector {
+func neighbors(pos Vector, garden [][]string) []Vector {
 	var neighbors []Vector
 	for x := -1; x <= 1; x++ {
 		for y := -1; y <= 1; y++ {
-			if (x == 0 && y == 0) || (x != 0 && y != 0){
+			if (x == 0 && y == 0) || (x != 0 && y != 0) {
 				continue
 			}
-			nx := pos.x + x
-			ny := pos.y + y
-			if nx < 0 || nx > len(garden[0])-1 || ny < 0 || ny > len(garden)-1 {
+			actualX := pos.x + x
+			actualY := pos.y + y
+			nx := actualX % len(garden[0])
+			ny := actualY % len(garden)
+			if nx < 0 {
+				nx = len(garden[0]) + nx
+			}
+			if ny < 0 {
+				ny = len(garden) + ny
+			}
+
+			if garden[ny][nx] != "." {
 				continue
 			}
-			if garden[ny][nx] != "." || distances[ny][nx] != -1 {
-				continue
-			}
-			neighbors = append(neighbors, Vector{nx, ny})
+			neighbors = append(neighbors, Vector{actualX, actualY})
 		}
 	}
 	return neighbors
 }
 
 // if the distance is a multiple of 2 it can be reached when the number of steps is also a multiple of 2
-func reachable(distances [][]int) int  {
+func reachable(distances map[Vector]int, steps int) int {
 	count := 0
-	for _, row := range distances {
-		for _, tile := range row {
-			if tile % 2 == 0 {
-				count++
-			}
+	for _, distance := range distances {
+		if distance%2 == steps % 2 {
+			count++
 		}
+
 	}
 	return count
 }
