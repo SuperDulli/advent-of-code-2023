7b52e10eee73072eca979ae3e3c1c079c5903482
chelmerd <chelmerd@student.42wolfsburg.de>
2023-12-29 14:39:11 +0000

    change day 18 part 1 to use shoelace and picks formula

--- 18_lava_duct_lagoon/main.go
+++ 18_lava_duct_lagoon/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"aoc2023/util"
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -12,32 +13,56 @@ type vector struct {
 }
 
 func main() {
-	lines := util.ReadLines("example.txt")
+	lines := util.ReadLines(os.Args[1])
 
-	pos := vector{0,0}
-	terrain := make(map[vector]int, 0)
+	pos := vector{0, 0}
+	corners := []vector{}
+	boundary := 0
 	for _, line := range lines {
-		terrain[pos] = 1
 		splitLine := strings.Fields(line)
 		direction := vector{}
 		switch splitLine[0] {
 		case "R":
-			direction = vector{1,0}
+			direction = vector{1, 0}
 		case "L":
-			direction = vector{-1,0}
+			direction = vector{-1, 0}
 		case "U":
-			direction = vector{0,-1}
+			direction = vector{0, -1}
 		case "D":
-			direction = vector{0,1}
+			direction = vector{0, 1}
 		}
 
 		// move
-		for i := 0; i < util.ConvertToNumber(splitLine[1]); i++ {
-			fmt.Println(pos)
-			pos.x += direction.x
-			pos.y += direction.y
-			terrain[pos] = 1
-		}
+		steps := util.ConvertToNumber(splitLine[1])
+		pos.x += steps * direction.x
+		pos.y += steps * direction.y
+		boundary += steps
+		corners = append(corners, pos)
 	}
-	fmt.Println(terrain) // boundary
+	if boundary%2 != 0 {
+		panic("boundary is not divisible by 2")
+	}
+
+	// shoelace formula
+	area := 0
+	for i := 0; i < len(corners); i++ {
+		x1index := i - 1
+		if x1index < 0 {
+			x1index = len(corners) - 1
+		}
+		x2index := i + 1
+		if x2index >= len(corners) {
+			x2index = 0
+		}
+		area += corners[i].y * (corners[x1index].x - corners[x2index].x)
+	}
+	if area%2 != 0 {
+		panic("area is not an integer")
+	}
+	area = area / 2
+
+	// Pick's theorem for points
+	inside := area - boundary/2 + 1
+
+	fmt.Println(inside + boundary)
 }
